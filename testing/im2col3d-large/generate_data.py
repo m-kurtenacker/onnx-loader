2033da46f3fd434c3e951c2fb7904ff5c7f9a65a
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2024-11-20 17:53:32 +0100

    Add test for im2col with stride and padding.

---
+++ testing/im2col3d-large/generate_data.py
@@ -0,0 +1,12 @@
+import idx2numpy
+import numpy as np
+
+#shape = (64, 16, 224, 224)
+shape = (64, 16, 224, 224)
+
+ndarr = np.random.random(shape)
+ndarr = np.array(ndarr, dtype=np.float32)
+
+print(ndarr.shape)
+
+idx2numpy.convert_to_file("test.idx", ndarr)
---
+++ testing/im2col3d-large/test.py
@@ -0,0 +1,106 @@
+import idx2numpy
+import numpy as np
+import sys
+import os
+import subprocess
+
+def get_indices(X_shape, HF, WF, stride, pad):
+    """
+        Returns index matrices in order to transform our input image into a matrix.
+
+        Parameters:
+        -X_shape: Input image shape.
+        -HF: filter height.
+        -WF: filter width.
+        -stride: stride value.
+        -pad: padding value.
+
+        Returns:
+        -i: matrix of index i.
+        -j: matrix of index j.
+        -d: matrix of index d. 
+            (Use to mark delimitation for each channel
+            during multi-dimensional arrays indexing).
+    """
+    # get input size
+    m, n_C, n_H, n_W = X_shape
+
+    # get output size
+    out_h = int((n_H + 2 * pad - HF) / stride) + 1
+    out_w = int((n_W + 2 * pad - WF) / stride) + 1
+  
+    # ----Compute matrix of index i----
+
+    # Level 1 vector.
+    level1 = np.repeat(np.arange(HF), WF)
+    # Duplicate for the other channels.
+    level1 = np.tile(level1, n_C)
+    # Create a vector with an increase by 1 at each level.
+    everyLevels = stride * np.repeat(np.arange(out_h), out_w)
+    # Create matrix of index i at every levels for each channel.
+    i = level1.reshape(-1, 1) + everyLevels.reshape(1, -1)
+
+    # ----Compute matrix of index j----
+    
+    # Slide 1 vector.
+    slide1 = np.tile(np.arange(WF), HF)
+    # Duplicate for the other channels.
+    slide1 = np.tile(slide1, n_C)
+    # Create a vector with an increase by 1 at each slide.
+    everySlides = stride * np.tile(np.arange(out_w), out_h)
+    # Create matrix of index j at every slides for each channel.
+    j = slide1.reshape(-1, 1) + everySlides.reshape(1, -1)
+
+    # ----Compute matrix of index d----
+
+    # This is to mark delimitation for each channel
+    # during multi-dimensional arrays indexing.
+    d = np.repeat(np.arange(n_C), HF * WF).reshape(-1, 1)
+
+    return i, j, d
+
+def im2col(X, HF, WF, stride, pad):
+    """
+        Transforms our input image into a matrix.
+
+        Parameters:
+        - X: input image.
+        - HF: filter height.
+        - WF: filter width.
+        - stride: stride value.
+        - pad: padding value.
+
+        Returns:
+        -cols: output matrix.
+    """
+    # Padding
+    X_padded = np.pad(X, ((0,0), (0,0), (pad, pad), (pad, pad)), mode='constant')
+    i, j, d = get_indices(X.shape, HF, WF, stride, pad)
+    # Multi-dimensional arrays indexing.
+    cols = X_padded[:, d, i, j]
+    cols = np.concatenate(cols, axis=-1)
+    return cols
+
+if len(sys.argv) > 1:
+    current_source_dir = os.path.dirname(sys.argv[0])
+    current_binary_dir = sys.argv[1]
+    subprocess.Popen([os.path.join(current_binary_dir, "test_im2col3d_large")]).communicate()
+
+a = idx2numpy.convert_from_file("test.idx")
+print(a)
+#a = np.array([a])
+result = im2col(a, 5, 5, 3, 2)
+print(result)
+
+print(result.shape)
+
+onnx_result = idx2numpy.convert_from_file("result.idx")
+print(onnx_result)
+
+compare_distance = np.absolute(result - onnx_result)
+print(compare_distance)
+
+if np.max(compare_distance) > 0.01:
+    sys.exit(1)
+else:
+    sys.exit(0)
